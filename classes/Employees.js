eba90df3846ba25b6712f45e1c7967fce4a3a5f6
pmolina90 <P.a.molina90@gmail.com>
2024-02-07 12:28:02 -0500

    added code to EMPLOYEE, all test passed

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,9 +1,33 @@
 class Employees {
-
-
-
+    #salary;
+    #isHired = true;
     
-}
+    constructor(name, title, salary){
+        this.name = name;
+        this.title = title;
+        this.#salary = salary;
+    }
+        getSalary(){
+            return this.#salary;
+        }
+    
+        setSalary(amount){
+            this.#salary = amount;
+        }
+    
+        getStatus(){
+            return this.#isHired;
+        }
+    
+        setStatus(command){
+            if (command === "fire"){
+                this.#isHired = false;
+            } else {
+                this.#isHired = true;
+            }
+        }
+    }
+
 
 module.exports = {
     Employees,
