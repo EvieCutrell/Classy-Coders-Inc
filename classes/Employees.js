5f07a368f811a4369d9abae893fb592aecdc89e8
emartincek1 <evan.martincek@gmail.com>
2024-02-07 12:05:56 -0600

    add salesPerson and add employee statics

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,34 +1,48 @@
 class Employees {
-    #salary;
-    #isHired = true;
-    
-    constructor(name, title, salary){
-        this.name = name;
-        this.title = title;
-        this.#salary = salary;
-    }
-        getSalary(){
-            return this.#salary;
-        }
-    
-        setSalary(amount){
-            this.#salary = amount;
-        }
-    
-        getStatus(){
-            return this.#isHired;
-        }
-    
-        setStatus(command){
-            if (command === "fire"){
-                this.#isHired = false;
-            } else {
-                this.#isHired = true;
-            }
-        }
-    }
+  #salary;
+  #isHired = true;
+  static #allEmployees = [];
 
+  constructor(name, title, salary) {
+    this.name = name;
+    this.title = title;
+    this.#salary = salary;
+    Employees.#allEmployees.push(this);
+  }
+  getSalary() {
+    return this.#salary;
+  }
+
+  setSalary(amount) {
+    this.#salary = amount;
+  }
+
+  getStatus() {
+    return this.#isHired;
+  }
+
+  setStatus(command) {
+    if (command === "fire") {
+      this.#isHired = false;
+    } else {
+      this.#isHired = true;
+    }
+  }
+
+  static getEmployees() {
+    return Employees.#allEmployees;
+  }
+
+  static getTotalPayroll() {
+    let total = 0;
+    Employees.#allEmployees.forEach((e) => {
+      let tempSalary = e.getSalary();
+      total += tempSalary;
+    });
+    return total;
+  }
+}
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
